d177028784f514f797b31dbb009a8f283d7292bb
H P <nurchumairohahmad@gmail.com>
2026-06-14 17:31:42 +0700

    new lagi

--- resources/views/components/wizard/applikasi-sub.blade.php
+++ resources/views/components/wizard/applikasi-sub.blade.php
@@ -729,7 +729,7 @@
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                                 d="M5 13l4 4L19 7"></path>
                                         </svg>
-                                        <span class="text-gray-600">Jaminan keananan data</span>
+                                        <span class="text-gray-600">Jaminan keamanan data</span>
                                     </li>
                                     <li class="flex items-start">
                                         <svg class="mr-2 mt-0.5 h-5 w-5 text-emerald-500" fill="none"
